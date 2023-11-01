085fc99b6fe9967d2465363a6928d899dde468ce
Simon Green Kristensen <git@simon-green.dev>
2023-11-01 23:00:45 +0100

    Working datastore!

--- function-src/index.ts
+++ function-src/index.ts
@@ -1,5 +1,27 @@
-import { http } from '@google-cloud/functions-framework';
+import { http } from "@google-cloud/functions-framework";
+import { Datastore } from "@google-cloud/datastore";
 
-http('helloHttp', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+const ds = new Datastore();
+
+http("splendor-test", async (req, res) => {
+  // The kind for the new entity
+  const kind = "Splendor Game State";
+
+  // The name/ID for the new entity
+  const name = req.query.name?.toString() || "default";
+
+  // The Cloud Datastore key for the new entity
+  const taskKey = ds.key([kind, name]);
+
+  // Prepares the new entity
+  const task = {
+    key: taskKey,
+    data: {
+      description: "Buy milk",
+    },
+  };
+
+  // Saves the entity
+  const x = await ds.save(task);
+  res.send(`Saved ${name}`);
 });
