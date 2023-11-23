4e51d66fa1ae05cf0ea74e0d656fc6adde5bea02
Simon Green Kristensen <git@simon-green.dev>
2023-11-23 23:23:43 +0100

    Improve cost locations on mobile

--- frontend/src/routes/test/Card.svelte
+++ frontend/src/routes/test/Card.svelte
@@ -2,9 +2,20 @@
 	import type { Card } from '../../../../common/model';
 	import { colorOf, gradientOf } from '$lib/color';
 	export let card: Card;
+	export let showCost = true;
 	const { c, cost, id, p } = card;
 	const filtered = cost.filter(Boolean);
 	const cols = [' grid-cols-1', ' grid-cols-1', ' grid-cols-2 md:grid-cols-1', ' grid-cols-2'];
+	let secondPos = -1;
+	for (let i = 0; i < cost.length; i++) {
+		if (cost[i]) {
+			if (secondPos !== -1) {
+				secondPos = i;
+				break;
+			}
+			secondPos = i;
+		}
+	}
 </script>
 
 <div
@@ -12,14 +23,17 @@
 		gradientOf[c]}
 >
 	<div class={'py-0.5 px-2 text-3xl md:text-6xl rounded-t-lg ' + colorOf[c]}>
-		<span class={p ? '' : ' opacity-20'}>{p ?? '0'}</span>
+		<span class={p ? '' : 'opacity-20'}>{p ?? '0'}</span>
 	</div>
 	<div
 		class={'text-sm md:text-3xl md:leading-5 pl-1 md:pl-2 pb-1 md:pb-2 grid w-12 md:w-20 gap-1 leading-none md:gap-2' +
 			cols[filtered.length - 1]}
 	>
 		{#each cost as co, i}
-			{#if co}
+			{#if filtered.length === 3 && secondPos === i}
+				<div class="md:hidden" />
+			{/if}
+			{#if showCost && co}
 				<span
 					class={'p-0.5 md:p-1 w-5 md:w-8 text-center border border-black rounded-full aspect-square outline outline-white outline-2 md:outline-[3px] ' +
 						colorOf[i]}
