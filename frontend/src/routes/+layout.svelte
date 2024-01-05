1507c28a9c8416f6a3acbc5e12779e3eeee63655
Simon Green Kristensen <git@simon-green.dev>
2024-01-05 22:50:26 +0100

    Convert some CSS to Tailwind

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -15,53 +15,18 @@
 	});
 </script>
 
-<div class="app">
+<div class="flex flex-col min-h-screen">
 	<Header />
 
-	<main>
+	<main class="box-border flex flex-col flex-1 w-full max-w-5xl p-4 mx-auto">
 		<QueryClientProvider client={queryClient}>
 			<slot />
 		</QueryClientProvider>
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<footer class="flex flex-col items-center justify-center p-3 min-[480px]:px-0">
+		<p>
+			visit <a class="font-bold" href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+		</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
