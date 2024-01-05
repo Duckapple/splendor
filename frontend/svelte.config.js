d137961d62f9cb6d0218b7b24dcd66a2f8b65bf0
Simon Green Kristensen <git@simon-green.dev>
2024-01-05 21:39:33 +0100

    Update everything frontend, get build to work

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,14 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html',
+		}),
 		files: {
-			serviceWorker: 'src/service-worker.ts'
-		}
-	}
+			serviceWorker: 'src/service-worker.ts',
+		},
+	},
 };
 
 export default config;
