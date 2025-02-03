79c6f14a99b53d53cdbff212b94e9a0d93a1e1d2
Simon Green Kristensen <git@simon-green.dev>
2025-02-03 22:28:48 +0100

    fix: deploy script actually fully deploys

--- deploy.ts
+++ deploy.ts
@@ -16,9 +16,13 @@ console.log('Build complete, transferring files...');
 await Promise.all([
 	$`scp ./.env pi@pi.local:~/splendor/.env`,
 	$`scp ./function/index.js pi@pi.local:~/splendor/index.js`,
-	$`scp -r ./frontend/build pi@pi.local:~/splendor/frontend`,
+	$`scp -r ./frontend/build pi@pi.local:~/splendor/frontend-copy`,
 ]);
 
-$`ssh pi@pi.local "cd ~/splendor/frontend && bun i && sudo systemctl restart splendor-frontend"`;
+await $`scp ./frontend/.env.production pi@pi.local:~/splendor/frontend-copy/.env`;
+
+await $`ssh pi@pi.local "rm -r ~/splendor/frontend && mv ~/splendor/frontend-copy ~/splendor/frontend"`;
+
+await $`ssh pi@pi.local "cd ~/splendor/frontend && ~/.bun/bin/bun i && sudo systemctl restart splendor-frontend && sudo systemctl restart splendor"`;
 
 console.log('Done!');
