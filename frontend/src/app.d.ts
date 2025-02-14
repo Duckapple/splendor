1f803441f073dde3c97a87cf73c73db7a2bf9840
Simon Green Kristensen <github@simon-green.dev>
2025-02-14 14:46:45 +0100

    feat: add cool type declarations

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -9,4 +9,9 @@ declare global {
 	}
 }
 
+declare interface String {
+  startsWith<T extends string>(searchString: T): this is `${T}${string}`;
+  includes<T extends string>(searchString: T): this is `${string}${T}${string}`;
+}
+
 export {};
