db4364a99e0bc688bcc60174058abfb13df74d8a
Simon Green Kristensen <git@simon-green.dev>
2025-03-03 18:23:50 +0100

    feat: ui page for creating marketing images

--- frontend/src/lib/compose/Background.svelte
+++ frontend/src/lib/compose/Background.svelte
@@ -3,7 +3,7 @@
 	import { onMount } from 'svelte';
 	import { cardFromId } from '../../../../common/defaults';
 
-	let { card = cardFromId(75) } = $props();
+	let { card = cardFromId(75), small = false, class: cn = '' } = $props();
 
 	function getBoundingTiles(toCos: number, toSin: number) {
 		const cos = Math.cos((Math.PI * 12) / 180) * toCos;
@@ -11,25 +11,33 @@
 		return cos + sin;
 	}
 
-	let cardSize = $state((globalThis.window?.innerWidth ?? 0) < 768 ? [54, 86] : [189, 285]);
+	let cardSize = $state(
+		(globalThis.window?.innerWidth ?? 0) < 768 || small ? [54, 86] : [189, 285]
+	);
 
-	let x = $derived(globalThis.window?.innerWidth
-		? Math.ceil(getBoundingTiles(window.innerWidth, window.innerHeight) / cardSize[0])
-		: 0);
-	let y = $derived(globalThis.window?.innerHeight
-		? Math.ceil(getBoundingTiles(window.innerHeight, window.innerWidth) / cardSize[1])
-		: 0);
+	let x = $derived(
+		globalThis.window?.innerWidth
+			? Math.ceil(getBoundingTiles(window.innerWidth, window.innerHeight) / cardSize[0])
+			: 0
+	);
+	let y = $derived(
+		globalThis.window?.innerHeight
+			? Math.ceil(getBoundingTiles(window.innerHeight, window.innerWidth) / cardSize[1])
+			: 0
+	);
 
-	let dX = $derived(globalThis.window?.innerWidth
-		? Math.sin((Math.PI * 12) / 180) ** 2 * window.innerWidth
-		: 0);
-	let dY = $derived(globalThis.window?.innerWidth
-		? Math.sin((Math.PI * 12) / 180) * Math.cos((Math.PI * 12) / 180) * window.innerWidth
-		: 0);
+	let dX = $derived(
+		globalThis.window?.innerWidth ? Math.sin((Math.PI * 12) / 180) ** 2 * window.innerWidth : 0
+	);
+	let dY = $derived(
+		globalThis.window?.innerWidth
+			? Math.sin((Math.PI * 12) / 180) * Math.cos((Math.PI * 12) / 180) * window.innerWidth
+			: 0
+	);
 
 	onMount(() => {
 		const cb = () => {
-			cardSize = globalThis.window.innerWidth < 768 ? [54, 86] : [189, 285];
+			cardSize = globalThis.window.innerWidth < 768 || small ? [54, 86] : [189, 285];
 		};
 		if (globalThis.window) {
 			window.addEventListener('resize', cb);
@@ -40,16 +48,22 @@
 
 <div class="fixed">
 	<div
-		class="absolute inset-0 z-20 w-screen bg-[radial-gradient(farthest-corner,_white,_lightgray)] opacity-50"
+		class={[
+			'absolute inset-0 z-20 w-screen bg-[radial-gradient(farthest-corner,_white,_lightgray)] opacity-50',
+			cn,
+		]}
 	></div>
 	<div
-		class="flex flex-col h-screen gap-2 overflow-hidden origin-top-left md:gap-4 bg-slate-50"
+		class={[
+			'flex flex-col h-screen gap-2 overflow-hidden origin-top-left bg-slate-50',
+			!small && 'md:gap-4',
+		]}
 		style="transform: translate({dX}px, -{dY}px) rotate(12deg) scale(1.5);"
 	>
 		{#each Array(y) as _}
-			<div class="flex gap-2 md:gap-4">
+			<div class={['flex gap-2', !small && 'md:gap-4']}>
 				{#each Array(x) as _}
-					<Card {card} />
+					<Card {card} {small} />
 				{/each}
 			</div>
 		{/each}
---
+++ frontend/src/routes/ui/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Background from '$lib/compose/Background.svelte';
+	import { cardFromId } from '../../../../common/defaults';
+</script>
+
+<Background card={cardFromId(143)} class="opacity-60" />
+
+<div
+	class="absolute inset-0 flex flex-col items-center justify-center [text-shadow:-1px_-1px_0_#fff,_1px_-1px_0_#fff,_-1px_1px_0_#fff,_1px_1px_0_#fff;]"
+>
+	<div class="w-[1200px] h-[675px] flex flex-col items-center justify-center">
+		<span class="text-9xl text-slate-950">You're invited!</span>
+		<span class="text-6xl">Click to join!</span>
+	</div>
+</div>
